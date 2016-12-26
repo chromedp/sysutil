7e40594fdcf82ff8690358447b0b4e26558897d6
Kenneth Shaw <kenneth.shaw@knq.io>
2016-12-26 09:30:00 +0700

    Fixing issues for BSD systems

--- sysutil_bsd.go
+++ sysutil_bsd.go
@@ -21,11 +21,11 @@ func init() {
 
 	// decode
 	var t syscall.Timeval32
-	err = binary.Read(bytes.NewBuffer([]byte(res), binary.LittleEndian), &t)
+	err = binary.Read(bytes.NewBuffer([]byte(res)), binary.LittleEndian, &t)
 	if err != nil {
 		btime = time.Now()
 		return
 	}
 
-	btime = time.Unix(t.Sec, t.Usec)
+	btime = time.Unix(int64(t.Sec), int64(t.Usec))
 }
